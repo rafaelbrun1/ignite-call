e9019255789ff3e7f65c88fd36eff8d7d38182af
Rafael Brun <rafael.brun@yahoo.com.br>
2023-01-09 22:29:09 -0300

    register finished

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -17,6 +17,10 @@ export default function ConnectCalendar() {
     await signIn('google')
   }
 
+
+  async function handleNavigateToNextStep() { 
+    await router.push('/register/time-intervals')
+  }
   return (
     <Container>
       <Header>
@@ -54,7 +58,7 @@ export default function ConnectCalendar() {
             permissões de acesso ao Google Calendar.
           </AuthError>
         )}
-        <Button type="submit" disabled={!isSignedIn}>
+        <Button onClick={handleNavigateToNextStep} type="submit" disabled={!isSignedIn}>
           Próximo passo
           <ArrowRight />
         </Button>
--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,6 +7,7 @@ import {
   Text,
   TextInput,
 } from "@ignite-ui/react";
+import { useRouter } from "next/router";
 import { ArrowRight } from "phosphor-react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
@@ -93,6 +94,8 @@ export default function TimeIntervals() {
     name: "intervals",
   });
 
+  const router = useRouter()
+
   const weekDays = getWeekDays();
 
   const intervals = watch("intervals");
@@ -103,6 +106,8 @@ export default function TimeIntervals() {
     await api.post("/users/time-intervals", {
       intervals,
     });
+
+    await router.push('/register/update-profile')
   }
 
   return (
