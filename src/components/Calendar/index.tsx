b8408792ba8c61b11f78b37f3ce058e4e982221a
Rafael Brun <rafael.brun@yahoo.com.br>
2023-01-13 15:33:14 -0300

    props

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -21,7 +21,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[];
 
-export function Calendar() {
+interface CalendarProps { 
+  selectedDate?: Date | null
+  onDateSelected: (date: Date) => void
+}
+
+export function Calendar({ selectedDate, onDateSelected}: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set("date", 1);
   });
@@ -63,6 +68,7 @@ export function Calendar() {
       "date",
       currentDate.daysInMonth()
     );
+    
     const lastWeekDay = lastDayInCurrentMonth.get("day");
 
     const nextMonthFillArray = Array.from({
@@ -76,7 +82,7 @@ export function Calendar() {
         return { date, disabled: true };
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false };
+        return { date, disabled: date.endOf('day').isBefore(new Date()) };
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true };
@@ -134,7 +140,7 @@ export function Calendar() {
                 {days.map(({ date, disabled }) => {
                   return (
                     <td key={date.toString()}>
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay onClick={() => onDateSelected(date.toDate())}disabled={disabled}>
                         {date.get("date")}
                       </CalendarDay>
                     </td>
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,35 +1,43 @@
+import { useState } from "react";
 import { Calendar } from "../../../../../components/Calendar";
-import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from "./styles";
 
-export function CalendarStep() { 
-  const isDateSelected = false
-  return ( 
-    <Container isTimePickerOpen={isDateSelected} > 
-      
-      <Calendar />
+export function CalendarStep() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const isDateSelected = !!selectedDate;
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar selectedDate={selectedDate} onDateSelected={setSelectedDate} />
 
-      {isDateSelected && (<TimePicker >
-      <TimePickerHeader> 
-        terça-feira <span>20 de setembro</span> 
-      </TimePickerHeader> 
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
 
-      <TimePickerList> 
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-        <TimePickerItem>08:00h</TimePickerItem>
-      </TimePickerList>
-      </TimePicker >
-      ) }
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
-  )
+  );
 }
